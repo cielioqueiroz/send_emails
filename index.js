05042f594ff11dffe1be287bbf5bfb94a075228c
Jacielio Queiroz <cielioqueiroz@hotmail.com>
2024-04-30 12:02:48 -0300

    Starting an email sending project in Node.js

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
+require('dotenv').config();
+
+const variablesUser = process.env.USER;
+const variablesPass = process.env.PASS;
+
 const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   host: 'smtp.office365.com',
   port: 587,
   secure: false, // true for 465, false for other ports
   auth: {
-    user: 'cielioqueiroz@hotmail.com',
-    pass: '21_a1219200@'
+    user: variablesUser,
+    pass: variablesPass
   }
 });
 
 transporter.sendMail({
-  from: 'Ciélio Queiroz <cielioqueiroz@hotmail.com>',
+  from: `${variablesUser} <cielioqueiroz@hotmail.com>`,
   to: 'cielioqueirozz@gmail.com',
   subject: 'Enviando email com Nodemailer',
   html: '<h1>Olá, Mundo!</h1> <p>Esse email foi enviado usando o Nodemailer.</p>',
